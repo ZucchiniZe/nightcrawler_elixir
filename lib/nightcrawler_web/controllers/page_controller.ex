652e659329045f60cd8311318ef91141791714b4
Alex B <me@alexb.io>
2018-04-06 01:58:54 -0700

    make comic_id optional

--- lib/nightcrawler_web/controllers/page_controller.ex
+++ lib/nightcrawler_web/controllers/page_controller.ex
@@ -6,7 +6,8 @@ defmodule NightcrawlerWeb.PageController do
     render conn, "index.html"
   end
 
-  def comics(conn, %{"comic_id" => id} = params) do
+  def comics(conn, params) do
+    id = Dict.get(params, "comic_id")
     case Marvel.get_comics(id) do
       {:ok, response} ->
         json conn, response.body
