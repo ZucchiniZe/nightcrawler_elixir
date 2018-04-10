d66bf3376110707ac39d099905fbddec33aa070f
Alex B <me@alexb.io>
2018-04-10 01:06:35 -0700

    add error handling and extract comic template

--- lib/nightcrawler_web/controllers/series_controller.ex
+++ lib/nightcrawler_web/controllers/series_controller.ex
@@ -4,19 +4,21 @@ defmodule NightcrawlerWeb.SeriesController do
   require Logger
 
   def all(conn, _params) do
-    case Marvel.get_series(nil, limit: 100) do
-      {:ok, response} ->
-        render(conn, "index.html", data: response.body["data"]["results"])
+    {:ok, response} = Marvel.get_series(nil, limit: 100)
 
-      {:error, error} ->
-        Logger.error(error)
+    if response.status == 200 do
+      render(conn, "index.html", data: response.body["data"]["results"])
+    else
+      conn
+      |> put_status(response.status)
+      |> json(response.body)
     end
   end
 
   def get(conn, %{"id" => id}) do
     case Marvel.get_series(id, []) do
       {:ok, response} ->
-        # json(conn, response.body["data"]["results"])
+        # returns a single element array so make sure to return that element
         render conn, "get.html", data: response.body["data"]["results"] |> List.first
 
       {:error, error} ->
