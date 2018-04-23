ab8d7e3149e60357dd573879d22d9e367c47bb3c
Alex B <me@alexb.io>
2018-04-23 04:17:37 -0700

    remove useless generated docs

--- apps/nightcrawler/lib/nightcrawler/marvel/marvel.ex
+++ apps/nightcrawler/lib/nightcrawler/marvel/marvel.ex
@@ -7,477 +7,135 @@ defmodule Nightcrawler.Marvel do
   alias Nightcrawler.Repo
   alias Nightcrawler.Marvel.{Comic, Series, Event, Creator, Character}
 
-  @doc """
-  Returns the list of series.
-
-  ## Examples
-
-      iex> list_series()
-      [%Series{}, ...]
-
-  """
   def list_series do
     Repo.all(Series)
   end
 
-  @doc """
-  Gets a single series.
-
-  Raises `Ecto.NoResultsError` if the Series does not exist.
-
-  ## Examples
-
-      iex> get_series!(123)
-      %Series{}
-
-      iex> get_series!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_series!(id), do: Repo.get!(Series, id)
 
-  @doc """
-  Creates a series.
-
-  ## Examples
-
-      iex> create_series(%{field: value})
-      {:ok, %Series{}}
-
-      iex> create_series(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_series(attrs \\ %{}) do
     %Series{}
     |> Series.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a series.
-
-  ## Examples
-
-      iex> update_series(series, %{field: new_value})
-      {:ok, %Series{}}
-
-      iex> update_series(series, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_series(%Series{} = series, attrs) do
     series
     |> Series.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Series.
-
-  ## Examples
-
-      iex> delete_series(series)
-      {:ok, %Series{}}
-
-      iex> delete_series(series)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_series(%Series{} = series) do
     Repo.delete(series)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking series changes.
-
-  ## Examples
-
-      iex> change_series(series)
-      %Ecto.Changeset{source: %Series{}}
-
-  """
   def change_series(%Series{} = series) do
     Series.changeset(series, %{})
   end
 
-  @doc """
-  Returns the list of comics.
-
-  ## Examples
-
-      iex> list_comics()
-      [%Comic{}, ...]
-
-  """
   def list_comics do
     Repo.all(Comic)
   end
 
-  @doc """
-  Gets a single comic.
-
-  Raises `Ecto.NoResultsError` if the Comic does not exist.
-
-  ## Examples
-
-      iex> get_comic!(123)
-      %Comic{}
-
-      iex> get_comic!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_comic!(id), do: Repo.get!(Comic, id)
 
-  @doc """
-  Creates a comic.
-
-  ## Examples
-
-      iex> create_comic(%{field: value})
-      {:ok, %Comic{}}
-
-      iex> create_comic(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_comic(attrs \\ %{}) do
     %Comic{}
     |> Comic.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a comic.
-
-  ## Examples
-
-      iex> update_comic(comic, %{field: new_value})
-      {:ok, %Comic{}}
-
-      iex> update_comic(comic, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_comic(%Comic{} = comic, attrs) do
     comic
     |> Comic.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Comic.
-
-  ## Examples
-
-      iex> delete_comic(comic)
-      {:ok, %Comic{}}
-
-      iex> delete_comic(comic)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_comic(%Comic{} = comic) do
     Repo.delete(comic)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking comic changes.
-
-  ## Examples
-
-      iex> change_comic(comic)
-      %Ecto.Changeset{source: %Comic{}}
-
-  """
   def change_comic(%Comic{} = comic) do
     Comic.changeset(comic, %{})
   end
 
-  @doc """
-  Returns the list of creators.
-
-  ## Examples
-
-      iex> list_creators()
-      [%Creator{}, ...]
-
-  """
   def list_creators do
     Repo.all(Creator)
   end
 
-  @doc """
-  Gets a single creator.
-
-  Raises `Ecto.NoResultsError` if the Creator does not exist.
-
-  ## Examples
-
-      iex> get_creator!(123)
-      %Creator{}
-
-      iex> get_creator!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_creator!(id), do: Repo.get!(Creator, id)
 
-  @doc """
-  Creates a creator.
-
-  ## Examples
-
-      iex> create_creator(%{field: value})
-      {:ok, %Creator{}}
-
-      iex> create_creator(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_creator(attrs \\ %{}) do
     %Creator{}
     |> Creator.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a creator.
-
-  ## Examples
-
-      iex> update_creator(creator, %{field: new_value})
-      {:ok, %Creator{}}
-
-      iex> update_creator(creator, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_creator(%Creator{} = creator, attrs) do
     creator
     |> Creator.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Creator.
-
-  ## Examples
-
-      iex> delete_creator(creator)
-      {:ok, %Creator{}}
-
-      iex> delete_creator(creator)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_creator(%Creator{} = creator) do
     Repo.delete(creator)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking creator changes.
-
-  ## Examples
-
-      iex> change_creator(creator)
-      %Ecto.Changeset{source: %Creator{}}
-
-  """
   def change_creator(%Creator{} = creator) do
     Creator.changeset(creator, %{})
   end
 
-  @doc """
-  Returns the list of characters.
-
-  ## Examples
-
-      iex> list_characters()
-      [%Character{}, ...]
-
-  """
   def list_characters do
     Repo.all(Character)
   end
 
-  @doc """
-  Gets a single character.
-
-  Raises `Ecto.NoResultsError` if the Character does not exist.
-
-  ## Examples
-
-      iex> get_character!(123)
-      %Character{}
-
-      iex> get_character!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_character!(id), do: Repo.get!(Character, id)
 
-  @doc """
-  Creates a character.
-
-  ## Examples
-
-      iex> create_character(%{field: value})
-      {:ok, %Character{}}
-
-      iex> create_character(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_character(attrs \\ %{}) do
     %Character{}
     |> Character.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a character.
-
-  ## Examples
-
-      iex> update_character(character, %{field: new_value})
-      {:ok, %Character{}}
-
-      iex> update_character(character, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_character(%Character{} = character, attrs) do
     character
     |> Character.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Character.
-
-  ## Examples
-
-      iex> delete_character(character)
-      {:ok, %Character{}}
-
-      iex> delete_character(character)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_character(%Character{} = character) do
     Repo.delete(character)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking character changes.
-
-  ## Examples
-
-      iex> change_character(character)
-      %Ecto.Changeset{source: %Character{}}
-
-  """
   def change_character(%Character{} = character) do
     Character.changeset(character, %{})
   end
 
-  @doc """
-  Returns the list of events.
-
-  ## Examples
-
-      iex> list_events()
-      [%Event{}, ...]
-
-  """
   def list_events do
     Repo.all(Event)
   end
 
-  @doc """
-  Gets a single event.
-
-  Raises `Ecto.NoResultsError` if the Event does not exist.
-
-  ## Examples
-
-      iex> get_event!(123)
-      %Event{}
-
-      iex> get_event!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_event!(id), do: Repo.get!(Event, id)
 
-  @doc """
-  Creates a event.
-
-  ## Examples
-
-      iex> create_event(%{field: value})
-      {:ok, %Event{}}
-
-      iex> create_event(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_event(attrs \\ %{}) do
     %Event{}
     |> Event.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a event.
-
-  ## Examples
-
-      iex> update_event(event, %{field: new_value})
-      {:ok, %Event{}}
-
-      iex> update_event(event, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_event(%Event{} = event, attrs) do
     event
     |> Event.changeset(attrs)
     |> Repo.update()
   end
 
-  @doc """
-  Deletes a Event.
-
-  ## Examples
-
-      iex> delete_event(event)
-      {:ok, %Event{}}
-
-      iex> delete_event(event)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_event(%Event{} = event) do
     Repo.delete(event)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking event changes.
-
-  ## Examples
-
-      iex> change_event(event)
-      %Ecto.Changeset{source: %Event{}}
-
-  """
   def change_event(%Event{} = event) do
     Event.changeset(event, %{})
   end
-
-  ##### MY CODE
 
   @doc """
   takes the raw api result and just bulk inserts into the database
