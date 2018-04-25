e1c668a5ece8b4bde5cf4edd34587f7a0119c481
Alex B <me@alexb.io>
2018-04-24 23:39:38 -0700

    im a dummy, put it in the wrong file

--- apps/nightcrawler/config/dev.exs
+++ apps/nightcrawler/config/dev.exs
@@ -1,11 +1,10 @@
 use Mix.Config
 
 # Configure your database
-# make tests work on semaphore
 config :nightcrawler, Nightcrawler.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
-  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
+  username: "postgres",
+  password: "postgres",
   database: "nightcrawler_dev",
   pool_size: 2
 
--- apps/nightcrawler/config/test.exs
+++ apps/nightcrawler/config/test.exs
@@ -1,10 +1,11 @@
 use Mix.Config
 
 # Configure your database
+# make tests work on semaphore
 config :nightcrawler, Nightcrawler.Repo,
   adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
   database: "nightcrawler_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
