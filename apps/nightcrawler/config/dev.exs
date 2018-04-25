b43f51a9cadecdd12a5c72f1e94e4bb07b1159b9
Alex B <me@alexb.io>
2018-04-24 23:34:20 -0700

    this should work?

--- apps/nightcrawler/config/dev.exs
+++ apps/nightcrawler/config/dev.exs
@@ -2,23 +2,12 @@ use Mix.Config
 
 # Configure your database
 # make tests work on semaphore
-unless System.get_env("CI") == "true" do
-  config :nightcrawler, Nightcrawler.Repo,
-    adapter: Ecto.Adapters.Postgres,
-    username: "postgres",
-    password: "postgres",
-    database: "nightcrawler_dev",
-    hostname: "localhost",
-    pool_size: 10
-else
-  config :nightcrawler, Nightcrawler.Repo,
-    adapter: Ecto.Adapters.Postgres,
-    username: System.get_env("DATABASE_POSTGRESQL_USERNAME"),
-    password: System.get_env("DATABASE_POSTGRESQL_PASSWORD"),
-    database: "nightcrawler_dev",
-    hostname: "localhost",
-    pool_size: 10
-end
+config :nightcrawler, Nightcrawler.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  username: System.get_env("DATABASE_POSTGRESQL_USERNAME") || "postgres",
+  password: System.get_env("DATABASE_POSTGRESQL_PASSWORD") || "postgres",
+  database: "nightcrawler_dev",
+  pool_size: 2
 
 config :nightcrawler, Nightcrawler.Repo,
   loggers: [{Ecto.LogEntry, :log, []},
