d48dbdd5d00a2ec0d706dfd8b59b6264743b6b8f
Alex B <me@alexb.io>
2018-04-29 02:53:07 -0700

    infinite timeout plus optimizing for less requests

--- apps/marvel/lib/marvel.ex
+++ apps/marvel/lib/marvel.ex
@@ -15,7 +15,7 @@ defmodule Marvel do
   plug(Tesla.Middleware.Headers, [{"User-Agent", "#{@app}/#{@version}"}])
   plug(Tesla.Middleware.DecodeJson)
   unless Mix.env() == :test, do: plug(Tesla.Middleware.Logger)
-  plug(Tesla.Middleware.Timeout, timeout: 10_000)
+  plug(Tesla.Middleware.Timeout, timeout: :infinity)
   plug(Marvel.Middleware.Tracing)
   plug(Marvel.Middleware.Cache)
   plug(Marvel.Middleware.Auth)
@@ -24,23 +24,27 @@ defmodule Marvel do
   For any resources that returns a collection that is more than 100 (enforced limit)
   """
   def get_all(url, limit \\ 100) do
-    initial_response = get!(url, query: [limit: 1])
-    total = initial_response.body["data"]["total"]
+    first_response = get!(url, query: [limit: limit])
+    total = first_response.body["data"]["total"]
 
-    # make a list of 1 to the returned total every n numbers defined by `limit`
-    # credo:disable-for-next-line Credo.Check.Refactor.PipeChainStart
-    :lists.seq(1, total, limit)
-    |> Enum.map(fn offset ->
-      Task.async(fn ->
-        # make a request to the same url just changing the offset, given to us by the parent map function
-        get(url, query: [limit: limit, offset: offset])
+    if total > limit do
+      # make a list of 1 to the returned total every n numbers defined by `limit`
+      # credo:disable-for-next-line Credo.Check.Refactor.PipeChainStart
+      :lists.seq(limit, total, limit)
+      |> Enum.map(fn offset ->
+        Task.async(fn ->
+          # make a request to the same url just changing the offset, given to us by the parent map function
+          get(url, query: [limit: limit, offset: offset])
+        end)
       end)
-    end)
-    |> Enum.map(&Task.await(&1, 10_000))
-    |> Enum.concat([{:ok, initial_response}])
-    # beacuse data.results returns an array of values we want to flatten that, hence the use of `Enum.flat_map/2`
-    # we can also assume that everything worked and didn't error
-    # TODO: error handling
-    |> Enum.flat_map(fn {:ok, resp} -> resp.body["data"]["results"] end)
+      |> Enum.map(&Task.await(&1, :infinity))
+      |> Enum.concat([{:ok, first_response}])
+      # beacuse data.results returns an array of values we want to flatten that, hence the use of `Enum.flat_map/2`
+      # we can also assume that everything worked and didn't error
+      # TODO: error handling
+      |> Enum.flat_map(fn {:ok, resp} -> resp.body["data"]["results"] end)
+    else
+      first_response.body["data"]["results"]
+    end
   end
 end
