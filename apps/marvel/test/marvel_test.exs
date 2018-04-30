27dd6d2f8995e4e6a1fd56d3c8f841d2f45ab1cc
Alex B <me@alexb.io>
2018-04-29 23:19:56 -0700

    remove the timeout tests because timeout is now infinite

--- apps/marvel/test/marvel_test.exs
+++ apps/marvel/test/marvel_test.exs
@@ -9,24 +9,4 @@ defmodule MarvelTest do
     Marvel.get("https://httpbin.org/")
     assert {:ok, true} = Cachex.exists?(:marvel_cache, "https://httpbin.org/?")
   end
-
-  test "doesn't timeout at 5 secods" do
-    assert _ = Marvel.get("https://httpbin.org/delay/5")
-  end
-
-  test "doesn't timeout at 7 seconds" do
-    assert _ = Marvel.get("https://httpbin.org/delay/7")
-  end
-
-  test "doesn't timeout at 9 seconds" do
-    assert _ = Marvel.get("https://httpbin.org/delay/9")
-  end
-
-  test "times out at 10 seconds" do
-    assert {:error, :timeout} = Marvel.get("https://httpbin.org/delay/10")
-  end
-
-  test "times out at 13 seconds" do
-    assert {:error, :timeout} = Marvel.get("https://httpbin.org/delay/13")
-  end
 end
