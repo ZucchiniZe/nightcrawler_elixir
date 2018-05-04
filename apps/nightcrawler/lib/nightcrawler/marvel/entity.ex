4dd9be1c387bce3fd1109a660ff7c4a46108ab98
Alex B <me@alexb.io>
2018-05-04 08:01:19 -0700

    more docs for transform

--- apps/nightcrawler/lib/nightcrawler/marvel/entity.ex
+++ apps/nightcrawler/lib/nightcrawler/marvel/entity.ex
@@ -1,11 +1,34 @@
 defmodule Nightcrawler.Marvel.Entity do
   @moduledoc """
-  All of the API entities should be contain the `api_to_changeset/1` function, this is enforcing that
+  All of the API entities should be contain the `api_to_changeset/1` function,
+  this is enforcing that
   """
 
   @doc """
   `transform` is a map that contains functions to run against the schema being
   returned by the api to transform it into schema compatible with our database
+
+  To use transform you need to define a transform function in the entity that
+  you want to transform. A transform function is a map that consists of keys
+  that target the corresponding string version from the marvel api result.
+
+  ### Example
+
+  For api result json
+
+      %{
+        "id" => 1234,
+        "title" => "I'm a title"
+      }
+
+  The transform function would be
+
+      def transform do
+        %{
+          id: &Parser.integer_or_string/1,
+          title: &Parser.integer_or_string/1
+        }
+      end
   """
   @callback transform :: %{
               required(atom) =>
